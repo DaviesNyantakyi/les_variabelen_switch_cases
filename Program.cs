d060d15fed879204c8a506e0b5792864eb3d8e61
DaviesNyantakyi <davies.nyantakyi@outlook.com>
2024-08-03 01:57:15 +0200

    switch cases csharp

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace BerekeningTarief
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            int leeftijd;
+
+
+            leeftijd = int.Parse(Console.ReadLine());
+
+            // Switch case
+            switch (leeftijd)
+            {
+                case 1:
+                    Console.WriteLine("juist");
+                    break;
+                case 2:
+                    Console.WriteLine("juist");
+                    break;
+                case 3:
+                    Console.WriteLine("juist");
+                    break;
+                default:
+                    Console.WriteLine("fout");
+                    break;
+            }
+
+
+
+
+           
+        }
+    }
+}
+
+
+
